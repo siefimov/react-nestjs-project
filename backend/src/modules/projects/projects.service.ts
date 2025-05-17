5da3a88917e4468bd3a4acbbb63310f2e30e52c6
Serhii <sergii.iefimov@gmail.com>
2025-05-17 13:58:33 +0300

    mpma-2: + project service

---
+++ backend/src/modules/projects/projects.service.ts
@@ -0,0 +1,49 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Project } from './project.entity';
+import { Repository } from 'typeorm';
+import { CreateProjectDto } from './dto/create-project.dto';
+import { UpdateProjectDto } from './dto/update-project.dto';
+
+@Injectable()
+export class ProjectsService {
+  constructor(
+    @InjectRepository(Project)
+    private projectsRepository: Repository<Project>,
+  ) {}
+
+  async create(createProjectDto: CreateProjectDto): Promise<Project> {
+    const project = this.projectsRepository.create(createProjectDto);
+    return this.projectsRepository.save(project);
+  }
+
+  async findAll(): Promise<Project[]> {
+    return this.projectsRepository.find();
+  }
+
+  async findOne(id: number): Promise<Project> {
+    const project = await this.projectsRepository.findOneBy({ id });
+
+    if (!project) {
+      throw new NotFoundException(`Project with ID ${id} not found`);
+    }
+
+    return project;
+  }
+
+  async update(
+    id: number,
+    updateProjectDto: UpdateProjectDto,
+  ): Promise<Project> {
+    await this.projectsRepository.update(id, updateProjectDto);
+    return this.findOne(id);
+  }
+
+  async remove(id: number): Promise<void> {
+    const result = await this.projectsRepository.delete({ id });
+
+    if (result.affected === 0) {
+      throw new NotFoundException(`User with ID ${id} not found`);
+    }
+  }
+}
