3c07f84377be66181675f502eec835bb78f524a6
Serhii <sergii.iefimov@gmail.com>
2025-05-17 16:27:17 +0300

    mpma-3: + create, update task dto

---
+++ backend/src/modules/tasks/dto/create-task.dto.ts
@@ -0,0 +1,33 @@
+import {
+  IsEnum,
+  IsInt,
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+  Length,
+  Min,
+} from 'class-validator';
+import { TaskStatus } from '../task.entity';
+
+export class CreateTaskDto {
+  @IsString()
+  @IsNotEmpty()
+  @Length(1, 100)
+  readonly title: string;
+
+  @IsOptional()
+  @IsString()
+  readonly description?: string;
+
+  @IsInt()
+  @Min(1)
+  readonly projectId: number;
+
+  @IsInt()
+  @Min(1)
+  readonly assignedUserId: number;
+
+  @IsOptional()
+  @IsEnum(TaskStatus)
+  readonly status?: TaskStatus;
+}
