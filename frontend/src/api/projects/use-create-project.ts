4a50473f5584d5ec0fb8b47c0be29e47f2d00cd8
Serhii <sergii.iefimov@gmail.com>
2025-05-21 23:31:38 +0300

    mpma-6: + useCreateProject

---
+++ frontend/src/api/projects/use-create-project.ts
@@ -0,0 +1,53 @@
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import type {
+  CreateProjectRequestDto,
+  CreateProjectResponseDto,
+  Project,
+} from '../../types';
+import { http } from '../http';
+import { projectQueryKeys } from './project-query-keys';
+
+const createProjectFn = async (newPropect: CreateProjectRequestDto) => {
+  const response = await http.post<CreateProjectResponseDto>(
+    '/projects',
+    newPropect,
+  );
+  return response;
+};
+
+export const useCreateProject = () => {
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationFn: createProjectFn,
+    onMutate: async newProject => {
+      await queryClient.cancelQueries({ queryKey: projectQueryKeys.all });
+      const previousProjects = queryClient.getQueryData(projectQueryKeys.all);
+
+      queryClient.setQueryData(projectQueryKeys.all, (old: Project[] = []) => [
+        ...old,
+        {
+          id: Date.now(),
+          ...newProject,
+          owner: null,
+          createdAt: new Date().toISOString(),
+          updatedAt: new Date().toISOString(),
+        },
+      ]);
+
+      return { previousProjects };
+    },
+    onSuccess: () => {}, // todo: add toastify
+    onError: (_error, _variables, context) => {
+      if (context?.previousProjects) {
+        queryClient.setQueryData(
+          projectQueryKeys.all,
+          context.previousProjects,
+        );
+      }
+    },
+    onSettled: () => {
+      queryClient.invalidateQueries({ queryKey: projectQueryKeys.all });
+    },
+  });
+};
