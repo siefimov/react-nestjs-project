aa23b6985d249f09109c65e6d29c6d5d5c24aef3
Serhii <sergii.iefimov@gmail.com>
2025-05-23 00:55:59 +0300

    mpma-6: + useEditProject hook

--- frontend/src/api/projects/index.ts
+++ frontend/src/api/projects/index.ts
@@ -2,3 +2,4 @@ export * from './use-get-projects';
 export * from './use-get-project';
 export * from './use-delete-project';
 export * from './use-create-project';
+export * from './use-edit-project';
