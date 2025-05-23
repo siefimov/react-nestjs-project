b85479eb65cb930e7912c194cc45069764900dd0
Serhii <sergii.iefimov@gmail.com>
2025-05-23 11:18:40 +0300

    mpma-7: + projects view styles

--- frontend/src/views/projects/projects.tsx
+++ frontend/src/views/projects/projects.tsx
@@ -2,15 +2,21 @@ import { Link } from 'react-router';
 import { useProjects } from '../../api';
 import { ProjectList } from '../../components';
 import { APP_ROUTES } from '../../constants';
+import styles from './projects.module.scss';
 
 export const Projects = () => {
   const projects = useProjects();
 
   return (
-    <div>
-      <div>
-        <h2>List of Projects</h2>
-        <Link to={APP_ROUTES.CREATE}>+ New</Link>
+    <div className={styles['projects-page']}>
+      <div className={styles['projects-page__header']}>
+        <h2 className={styles['projects-page__title']}>List of Projects</h2>
+        <Link
+          to={APP_ROUTES.CREATE}
+          className={styles['projects-page__new-link']}
+        >
+          + New
+        </Link>
       </div>
       {projects.isSuccess && (
         <div>
