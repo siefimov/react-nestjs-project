7c85110c6a2d35049196b9cbafe8cbcbbd810750
Serhii <sergii.iefimov@gmail.com>
2025-05-25 16:50:15 +0300

    mpma-10: + add TaskUpdateSchema and TaskUpdateDto

--- frontend/src/schemas/task/task.schems.ts
+++ frontend/src/schemas/task/task.schems.ts
@@ -4,7 +4,7 @@ export const TaskStatusEnum = z.enum(['todo', 'in_progress', 'done']);
 
 export const TaskSchema = z.object({
   id: z.number(),
-  title: z.string().max(100),
+  title: z.string().min(1).max(100),
   description: z.string().nullable(),
   projectId: z.number(),
   assignedUserId: z.number().nullable(),
@@ -21,6 +21,13 @@ export const TaskCreateSchema = z.object({
   status: TaskStatusEnum,
 });
 
-export const TaskUpdateSchema = TaskCreateSchema.partial();
+export const TaskUpdateSchema = TaskCreateSchema.omit({
+  projectId: true,
+})
+  .partial()
+  .extend({
+    id: z.number().int().min(1, 'ID is required'),
+  });
 
 export type Task = z.infer<typeof TaskSchema>;
+export type TaskUpdateDto = z.infer<typeof TaskUpdateSchema>;
