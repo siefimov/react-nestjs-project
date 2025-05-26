a3cf9bda40672492ea17daa87b3a2a8f57d50d97
Serhii <sergii.iefimov@gmail.com>
2025-05-26 19:47:36 +0300

    mpma-14: + find-by-email to user service

--- backend/src/modules/users/users.service.ts
+++ backend/src/modules/users/users.service.ts
@@ -31,6 +31,10 @@ export class UsersService {
     return user;
   }
 
+  async findByEmail(email: string): Promise<User | null> {
+    return await this.usersRepository.findOne({ where: { email } });
+  }
+
   async update(id: number, updateUserDto: UpdateUserDto): Promise<User> {
     await this.usersRepository.update(id, updateUserDto);
     return this.findOne(id);
