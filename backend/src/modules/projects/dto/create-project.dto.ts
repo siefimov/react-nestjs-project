caaebd653d4c81c690971b5cbabc310ee5494ca3
Serhii <sergii.iefimov@gmail.com>
2025-05-26 13:06:50 +0300

    mpma-12: + implement Swagger API documentation with @nestjs/swagger

--- backend/src/modules/projects/dto/create-project.dto.ts
+++ backend/src/modules/projects/dto/create-project.dto.ts
@@ -6,17 +6,21 @@ import {
   Length,
   Min,
 } from 'class-validator';
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
 
 export class CreateProjectDto {
+  @ApiProperty({ example: 'Project title', maxLength: 100 })
   @IsString()
   @IsNotEmpty()
   @Length(1, 100)
   title: string;
 
+  @ApiPropertyOptional({ example: 'Project description' })
   @IsString()
   @IsOptional()
   description?: string;
 
+  @ApiProperty({ example: 2 })
   @IsInt()
   @Min(1)
   ownerId: number;
