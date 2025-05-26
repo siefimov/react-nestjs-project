028805f3b6192bdfd98d93ecb3bb480b37775c59
Serhii <sergii.iefimov@gmail.com>
2025-05-26 13:33:49 +0300

    mpma-14: + auth nodule, controller, service

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -6,6 +6,9 @@ import { AppService } from './app.service';
 import { UsersModule } from './modules/users/users.module';
 import { ProjectsModule } from './modules/projects/projects.module';
 import { TasksModule } from './modules/tasks/tasks.module';
+import { AuthModule } from './auth/auth.module';
+import { AuthModule } from './modules/auth/auth.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
@@ -22,6 +25,7 @@ import { TasksModule } from './modules/tasks/tasks.module';
     UsersModule,
     ProjectsModule,
     TasksModule,
+    AuthModule,
   ],
   controllers: [AppController],
   providers: [AppService],
