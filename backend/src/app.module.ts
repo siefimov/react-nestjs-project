d2b0ac85b9ec960fc1f17d68ba5cfe06db974f58
Serhii <sergii.iefimov@gmail.com>
2025-05-26 17:12:38 +0300

    mpma-14: * update import in app.module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -6,9 +6,7 @@ import { AppService } from './app.service';
 import { UsersModule } from './modules/users/users.module';
 import { ProjectsModule } from './modules/projects/projects.module';
 import { TasksModule } from './modules/tasks/tasks.module';
-import { AuthModule } from './auth/auth.module';
 import { AuthModule } from './modules/auth/auth.module';
-import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
