d11403086e5a776a48ff74e6ce2c902e55b7b798
Serhii <sergii.iefimov@gmail.com>
2025-05-28 00:36:27 +0300

    mpma-20: * refactor app-layout component

--- frontend/src/components/app-layout/app-layout.tsx
+++ frontend/src/components/app-layout/app-layout.tsx
@@ -1,35 +1,14 @@
-import { Outlet, useNavigate } from 'react-router';
-import clsx from 'clsx';
+import { Outlet } from 'react-router';
 import { useAuthInit } from '../../api';
-import { useAuthStore } from '../../store/auth-store';
-import { APP_ROUTES } from '../../constants';
 import styles from './app-layout.module.scss';
+import { Header } from '../header';
 
 export const AppLayout = () => {
   useAuthInit();
-  const { user, logout } = useAuthStore(state => state);
-  const navigte = useNavigate();
-
-  const handleLogout = () => {
-    logout();
-    navigte(APP_ROUTES.LOGIN);
-  };
 
   return (
     <div className={styles['app-layout']}>
-      <header className={clsx(styles['app-layout__header'], styles['header'])}>
-        <h1 className={styles['header__title']}>Mini Project Management App</h1>
-        {user && (
-          <div className={styles['header__user-block']}>
-            <span className={styles['header__name']}>
-              Welcome, {user.name}!
-            </span>
-            <button className={styles['header__logout']} onClick={handleLogout}>
-              Logout
-            </button>
-          </div>
-        )}
-      </header>
+      <Header />
       <Outlet />
     </div>
   );
