1f3b43abe16387d7af9a8039c1f94abe24daf18c
Serhii <sergii.iefimov@gmail.com>
2025-05-27 18:08:14 +0300

    mpma-17: change import to use Sass

--- frontend/src/components/private-routes/private-routes.tsx
+++ frontend/src/components/private-routes/private-routes.tsx
@@ -1,6 +1,5 @@
 import { Navigate, Outlet } from 'react-router';
 import { APP_ROUTES } from '../../constants';
-import { AppLayout } from '../app-layout';
 
 export const PrivateRoute = () => {
   const token = localStorage.getItem('token');
