58d92e90706525dd33d927ad90c5f1a7863eda34
Serhii <sergii.iefimov@gmail.com>
2025-05-27 12:29:44 +0300

    mpma-15: * routing-provider

--- frontend/src/providers/routing-provider.tsx
+++ frontend/src/providers/routing-provider.tsx
@@ -4,18 +4,28 @@ import {
   Route,
   RouterProvider,
 } from 'react-router';
-import { AppLayout } from '../components';
-import { Projects, Project, CreateProject } from '../views';
+import { AppLayout, PrivateLayout, PrivateRoute } from '../components';
+import { Projects, Project, CreateProject, Login, Register } from '../views';
 import { APP_ROUTES } from '../constants';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route element={<AppLayout />}>
-      <Route index element={<Projects />} />
-      <Route path={APP_ROUTES.PROJECT(':id')} element={<Project />} />
-      <Route path={APP_ROUTES.PROJECT_CREATE} element={<CreateProject />} />
-      <Route path={APP_ROUTES.EDIT(':id')} element={<div>Edit Project</div>} />
-      <Route path="*" element={<div>Not Found Page</div>} />
+      <Route path={APP_ROUTES.LOGIN} element={<Login />} />
+      <Route path={APP_ROUTES.REGISTER} element={<Register />} />
+
+      <Route element={<PrivateRoute />}>
+        <Route element={<PrivateLayout />}>
+          <Route index element={<Projects />} />
+          <Route path={APP_ROUTES.PROJECT(':id')} element={<Project />} />
+          <Route path={APP_ROUTES.PROJECT_CREATE} element={<CreateProject />} />
+          <Route
+            path={APP_ROUTES.EDIT(':id')}
+            element={<div>Edit Project</div>}
+          />
+          <Route path="*" element={<div>Not Found Page</div>} />
+        </Route>
+      </Route>
     </Route>,
   ),
 );
