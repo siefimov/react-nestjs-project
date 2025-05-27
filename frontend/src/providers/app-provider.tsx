72796bcff000421cbe0f004b815cc12878996dfa
Serhii <sergii.iefimov@gmail.com>
2025-05-28 00:37:55 +0300

    mpma-20: * change toast place

--- frontend/src/providers/app-provider.tsx
+++ frontend/src/providers/app-provider.tsx
@@ -6,7 +6,7 @@ export const AppProvider = () => {
   return (
     <ReactQueryProvider>
       <RoutingProvider />
-      <ToastContainer position="top-right" autoClose={3000} />
+      <ToastContainer position="bottom-right" autoClose={3000} />
     </ReactQueryProvider>
   );
 };
