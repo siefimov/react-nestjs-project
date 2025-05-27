05170145cf8aab1474bbc4526fe1c01c0cbb7c9b
Serhii <sergii.iefimov@gmail.com>
2025-05-27 12:28:40 +0300

    mpma-15: + register, login components

--- frontend/src/components/index.ts
+++ frontend/src/components/index.ts
@@ -3,4 +3,9 @@ export { ProjectList } from './project-list';
 export { ProjectInfo } from './project-info';
 export { TaskList } from './task-list';
 export { DeleteModal } from './delete-modal';
+export { LoginForm } from './login-form';
+export { RegisterForm } from './register-form';
+export { PrivateLayout } from './private-layout';
+export { PrivateRoute } from './private-routes/private-routes';
+
 export * from './icons';
---
+++ frontend/src/views/auth/register.tsx
@@ -0,0 +1,3 @@
+import { RegisterForm } from '../../components';
+
+export const Register = () => <RegisterForm />;
