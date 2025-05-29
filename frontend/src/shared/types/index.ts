c57365ab86267794b2096151b0dbead533054d56
Serhii <sergii.iefimov@gmail.com>
2025-05-29 12:49:17 +0300

    mpma-22: + add shared types

---
+++ frontend/src/shared/types/index.ts
@@ -0,0 +1,5 @@
+// export * from './user';
+export * from '../../types/project';
+export * from '../../types/task';
+export * from './app-routes.type';
+export * from './http-methods.type';
--- frontend/src/types/index.ts
+++
@@ -1,5 +0,0 @@
-export * from './user';
-export * from './project';
-export * from './task';
-export * from './app-routes.type';
-export * from './http-methods.type';
