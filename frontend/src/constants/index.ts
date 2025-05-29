9dfd3e789360b6d4c6b2eefe735b5287e410366a
Serhii <sergii.iefimov@gmail.com>
2025-05-29 12:52:56 +0300

    mpma-22: + add shared constants

--- frontend/src/constants/index.ts
+++
@@ -1,5 +0,0 @@
-export * from './app-routes.const';
-export * from './http-methods.const';
-export * from './editable-project-fileds.const';
-export * from './api-routes.const';
-export * from './task-status.const';
---
+++ frontend/src/shared/constants/index.ts
@@ -0,0 +1,5 @@
+export * from './app-routes.const';
+export * from './http-methods.const';
+export * from '../../constants/editable-project-fileds.const';
+export * from './api-routes.const';
+export * from '../../constants/task-status.const';
